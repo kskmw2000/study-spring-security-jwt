072dab614a02eba2e98a52417f80348dba334bd2
angeloper <minupurer@gmail.com>
2022-01-15 01:31:31 +0900

    add security filter and custom filter

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Security 개념잡기.
+- inflearn 에서 스프링부트 시큐리티 & jwt 강의를 보고 실습한 사항입니다.
+- 강의 주소는 https://www.inflearn.com/course/스프링부트-시큐리티 를 참고하였습니다.
--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.example.jwt.config;
 
+import com.example.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(corsConfig.corsFilter())     //  @CrossOrigin(인증X), 필터에 등록 => 인증(O)
                 .formLogin().disable()
-                .httpBasic().disable()
+                .httpBasic().disable()                  // header에 authorization에 ID와 PW를 넣어서 가져가는 방식.(http의 경우에 노출), Token의 경우에는 Bearer 방식임.
                 .authorizeHttpRequests()
                 .antMatchers("/api/v1/user/**").hasAnyRole("ROLE_USER", "ROLE_MANAGER", "ROLE_ADMIN")
                 .antMatchers("/api/v1/manager/**").hasAnyRole("ROLE_USER", "ROLE_MANAGER")
